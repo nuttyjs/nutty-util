96af09f67e67a34961cd44666d9c20d3ddd4de4b
jmjuanes <josemijuanes@gmail.com>
2017-01-27 20:40:14 +0100

    index.js: init

---
+++ index.js
@@ -0,0 +1,11 @@
+//Check a variable type
+module.exports.is = require('./lib/is.js');
+
+//Parse a variable
+module.exports.parse = require('./lib/parse.js');
+
+//Print a pretty json
+module.exports.pretty = require('./lib/pretty.js');
+
+//Exports the tamplate method
+module.exports.template = require('./lib/template.js');
