2f93ada4849025233c1ba7b5a92efb26e4964ee9
jmjuanes <josemijuanes@gmail.com>
2017-02-07 13:04:22 +0100

    index.js: added file and project

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 //Check a variable type
 module.exports.is = require('./lib/is.js');
 
+//Generate a file manager
+module.exports.file = require('floby');
+
 //Parse a variable
 module.exports.parse = require('./lib/parse.js');
+
+//Build a project object
+module.exports.project = require('./lib/project.js');
 
 //Exports the tamplate method
 module.exports.template = require('./lib/template.js');
