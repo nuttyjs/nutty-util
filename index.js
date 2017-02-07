0c633be9ee866940889ace9fa8807c96d67c5d8c
jmjuanes <josemijuanes@gmail.com>
2017-02-07 12:57:24 +0100

    index.js: removed unused lib

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ module.exports.is = require('./lib/is.js');
 //Parse a variable
 module.exports.parse = require('./lib/parse.js');
 
-//Print a pretty json
-module.exports.pretty = require('./lib/pretty.js');
-
 //Exports the tamplate method
 module.exports.template = require('./lib/template.js');
